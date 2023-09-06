d6abd3745a21b4e2efadc908bd8a3069a93be701
Meshak2002 <meshak2002@gmail.com>
2023-09-06 23:44:54 +0530

    WarmHole-60%

--- Assets/Warmhole.cs
+++ Assets/Warmhole.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Warmhole : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject pTunnel;
+    private GameObject cTunnel;
+    [SerializeField] private float pSpeed;
+    [SerializeField] private float cSpeed;
     void Start()
     {
-        
+        pTunnel = this.gameObject;
+        cTunnel = this.transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pTunnel.transform.Rotate(Vector3.up* pSpeed * Time.deltaTime);
+        cTunnel.transform.Rotate(Vector3.forward * cSpeed * Time.deltaTime);
     }
 }
